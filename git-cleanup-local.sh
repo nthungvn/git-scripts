f6d83809bb0221b501d004ae39097709f011465f
Hung Nguyen Thai <hung.nguyenthai@axonactive.com>
2019-10-25 10:40:43 +0700

    remove goto way

--- git-cleanup-local.sh
+++ git-cleanup-local.sh
@@ -1,13 +1,3 @@
 echo off
 
-goto() {
-    git branch --merged | grep -v -e 'master$' -e 'develop$' -e '1\.x\.x$' -e '2\.x\.x$' | xargs -L1 git branch -d
-}
-
-goto $@
-exit
-
-
-: () {
-echo %OS%
-exit
+git branch --merged | grep -v -e 'master$' -e 'develop$' -e '1\.x\.x$' -e '2\.x\.x$' | xargs -L1 git branch -d
