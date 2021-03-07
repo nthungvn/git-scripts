b524738956b602b1be6ae31a54d2d9774c24dea7
Hung Nguyen <nthung.vlvn@gmail.com>
2021-03-07 15:53:13 +0700

    update the protected branch

--- git-cleanup-remote-server.sh
+++ git-cleanup-remote-server.sh
@@ -1,4 +1,4 @@
 echo off
 
 git fetch -p origin
-git branch -r --merged | grep -v -e 'master$' -e 'develop$' -e '1\.x\.x$' -e '2\.x\.x$' | sed 's@origin/@@' | xargs -L1 git push origin -d
+git branch -r --merged | grep -v -e 'master$' -e 'develop$' -e 'main$' | sed 's@origin/@@' | xargs -L1 git push origin -d
